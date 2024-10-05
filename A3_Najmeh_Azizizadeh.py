0da8dd31d59aa3e907df6bbea31bc94ae1dcde58
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2024-10-05 12:34:08 +0330

    Update A3_Najmeh_Azizizadeh.py
    adding 3 line --> Import libs, import data, step1--> x and y

--- A3_Najmeh_Azizizadeh.py
+++ A3_Najmeh_Azizizadeh.py
@@ -3,15 +3,20 @@ Created on Fri Oct  4 23:51:38 2024
 
 @author: Najmeh Azizizadeh
 """
+
+#-----------Import Libs----------------------
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 from sklearn.datasets import fetch_california_housing
 
+#-----------Import Data----------------------
 data=fetch_california_housing()
 
 
+
+#-----------Step1 : X and Y ----------------------
 
 x=data.data
 y=data.target
