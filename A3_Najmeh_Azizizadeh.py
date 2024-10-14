c45737c6351c32bf3f425c8fc41cec414cd11519
Mathphd-ml <nazizizadeh@yahoo.com>
2024-10-15 00:02:03 +0330

    Update A3_Najmeh_Azizizadeh.py

--- A3_Najmeh_Azizizadeh.py
+++ A3_Najmeh_Azizizadeh.py
@@ -5,7 +5,9 @@ Created on Fri Oct  4 23:51:38 2024
 
 
 Salam Ostad
-ba dastoorati farmoodid kheili behtar shod sorat run.
+darsadhay jadid ro neveshtam.
+kheili run gereftam. Inha behtarin boodan, vali hanooz be deghat 90% nareside.
+mishe lotfan negahi befarmaeed.
 ************
 APM:
 dar ghesmate SVR man yek raveshe jadid zadam ( normal kardane data ha) , hala run konid va agar darsadeton kamtar az -0.1 bod
@@ -71,8 +73,8 @@ gs=GridSearchCV(model,my_params,cv=kf,scoring='neg_mean_absolute_percentage_erro
 
 #-----------Step4:  fit -------------------------------
 gs.fit(x,y)
-gs.best_score_     #np.float64(-0.0.478347758446281)
-gs.best_params_    #{'metric': 'manhattan', 'n_neighbors': 5, 'weights': 'distance'}
+gs.best_score_     #np.float64(-0.21067448749397003)
+gs.best_params_    #{'metric': 'manhattan', 'n_neighbors': 7, 'weights': 'distance'}
 cv=gs.cv_results_
 
 #------------------------------------------------------------------------------
@@ -80,7 +82,7 @@ cv=gs.cv_results_
 
 model=DecisionTreeRegressor(random_state=42)
 
-my_params={ 'max_depth':[1,2,3,24,21,17,22,10],
+my_params={ 'max_depth':[1,2,3,24,21,17,32,10],
            'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
            'splitter':['best','random'],'max_leaf_nodes':[798,810,805]}
 
@@ -88,14 +90,17 @@ my_params={ 'max_depth':[1,2,3,24,21,17,22,10],
 gs=GridSearchCV(model,my_params,cv=kf,scoring='neg_mean_absolute_percentage_error')
 
 #-----------Step4:  fit -------------------------------
-gs.fit(x,y)
-gs.best_score_     #np.float64(-0.22072829758907314)
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler() 
+X_scaled = scaler.fit_transform(x)
+gs.fit(X_scaled,y)
+gs.best_score_     #np.float64(-0.2202894825724071)
 gs.best_params_   
 '''
  {'criterion': 'absolute_error',
- 'max_depth': 21,
+ 'max_depth': 24,
  'max_leaf_nodes': 805,
- 'splitter': 'random'}
+ 'splitter': 'best'}
  '''
 cv=gs.cv_results_
 
@@ -104,30 +109,36 @@ cv=gs.cv_results_
 
 model=RandomForestRegressor(random_state=42)
 
-my_params={ 'n_estimators':[10,20,30,70,90],
+my_params={ 'n_estimators':[10,20,30,70,100],
            'max_features':[1,2,3,4,5,6,7],
            'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-           'max_depth':[2,3,4,25]}
+           'max_depth':[20,30,25]}
 
 
 
-gs=GridSearchCV(model,my_params,cv=kf,scoring='neg_mean_absolute_percentage_error')
+gs=GridSearchCV(model,my_params,cv=kf,n_jobs=-1,scoring='neg_mean_absolute_percentage_error')
 
 #-----------Step4:  fit -------------------------------
-gs.fit(x,y)
-gs.best_score_   #   np.float64(-0.1818179719877024)
-gs.best_params_    #{'max_depth': 25, 'max_features': 4, 'n_estimators': 90}
+gs.fit(X_scaled,y)
+gs.best_score_   #   np.float64(-0.17956506039641587)
+gs.best_params_    
+'''
+{'criterion': 'poisson',
+ 'max_depth': 20,
+ 'max_features': 7,
+ 'n_estimators': 100}
+'''
 cv=gs.cv_results_
 
 #------------------------------------------------------------------------------
 #--------------- Step3: Model 5 -------------------------
 
 model=SVR()
-my_params={'kernel':['poly','rbf','linear'],
-           'C':[0.001,0.01,1]}
+my_params={'kernel':['poly','rbf','linear'], 'epsilon':[0.01,0.00001], 'tol':[0.000001],
+           'C':[0.001,0.01],'degree':[2,3,7],'gamma':['scale','auto'],
+           'cache_size':[100,200],'coef0':[2,1]}
 
-
-gs=GridSearchCV(model,my_params,cv=kf,scoring='neg_mean_absolute_percentage_error')
+gs=GridSearchCV(model,my_params,cv=kf,n_jobs=-1,scoring='neg_mean_absolute_percentage_error')
 
 #-----------Step4:  fit -------------------------------
 #******
@@ -136,12 +147,19 @@ scaler=MinMaxScaler()
 x_scaled=scaler.fit_transform(x) 
 
 
-#gs.fit(x,y)
-#hala fit mikonim b x_scaled ke x haro too range 0 ta 1 ovordim
-#*** in kar baes mishe hame dataha normal beshan ham sorat behtar mishe ham deghat
 gs.fit(x_scaled,y)
-gs.best_score_     #np.float64(-0.29294216082111857)
-gs.best_params_    #{'C': 0.01, 'kernel': 'linear'}   
+gs.best_score_     #np.float64(-0.2346791171309992)
+gs.best_params_
+'''
+{'C': 0.01,
+ 'cache_size': 100,
+ 'coef0': 2,
+ 'degree': 7,
+ 'epsilon': 0.01,
+ 'gamma': 'scale',
+ 'kernel': 'poly',
+ 'tol': 1e-06}
+''' 
 cv=gs.cv_results_
 
 
